03cd6bc466de5e0976910d764fb3816d2bdbfb62
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-05-15 23:15:19 -0300

    Configuracion Vuex

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -3,6 +3,15 @@ export default {
     component: () =>
         import ( /* webpackChunkName: "daybook" */ '@/modules/daybook/layouts/DayBookLayout'),
     children: [
-
+        {
+            path: '',
+            name: 'no-entry',
+            component: () => import ( /* webpackChunkName: "daybook-no-entry" */ '@/modules/daybook/views/NoEntrySelected')
+        },
+        {
+            path: ':id',
+            name: 'entry',
+            component: () => import ( /* webpackChunkName: "daybook-entry" */ '@/modules/daybook/views/EntryView')
+        }
     ]
 }
