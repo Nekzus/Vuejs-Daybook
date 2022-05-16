58ff417ab6fccab7ecda5ea01c0b712fe40f29fb
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-05-16 19:42:33 -0300

    Backup Codigo Fuente

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4,5,6]
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Quis enim duis excepteur mollit occaecat est eu enim est incididunt elit ad.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Excepteur cillum consectetur qui excepteur sunt ullamco voluptate do cillum tempor laboris pariatur dolore in.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Elit ex veniam tempor culpa duis labore ad qui Lorem eiusmod adipisicing duis.',
+            picture: null,
+        },
+    ]
 })
