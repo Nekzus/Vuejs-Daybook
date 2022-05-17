f5789ae4a81f9323fc8c33bf840f2dd01f846b19
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-05-16 23:19:21 -0300

    Busqueda de entradas

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,19 +2,19 @@ export default () => ({
     isLoading: true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
             text: 'Quis enim duis excepteur mollit occaecat est eu enim est incididunt elit ad.',
             picture: null,
         },
         {
-            id: new Date().getTime() + 1000,
+            id: '2',
             date: new Date().toDateString(),
             text: 'Excepteur cillum consectetur qui excepteur sunt ullamco voluptate do cillum tempor laboris pariatur dolore in.',
             picture: null,
         },
         {
-            id: new Date().getTime() + 2000,
+            id: '3',
             date: new Date().toDateString(),
             text: 'Elit ex veniam tempor culpa duis labore ad qui Lorem eiusmod adipisicing duis.',
             picture: null,
