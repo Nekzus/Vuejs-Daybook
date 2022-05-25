d875e4a6aba4e0dafb1b307e612fe5ac68c7ad8f
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-05-25 18:10:44 -0300

    Testing Vuex

--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -1,16 +1,15 @@
+import 'setimmediate'
 import cloudinary from 'cloudinary'
 import axios from 'axios'
 import uploadImage from '@/modules/daybook/helpers/uploadImage'
-import 'setimmediate'
 
 cloudinary.config({
     cloud_name: 'dsvsl0b0b',
     api_key: '725288349447336',
     api_secret: 'yE8HujoLEJezgddssetQUQTVybw'
 })
-
 describe('Pruebas en el uploadImage', () => {
-    test('debe de cargar un archivo y retornar el url', async () => {
+    test('debe de cargar un archivo y retornar el url', async (done) => {
         const { data } = await axios.get('https://res.cloudinary.com/dsvsl0b0b/image/upload/v1653346900/uu4crmvkmv2tk3ntze9y.jpg', {
             responseType: 'arraybuffer'
         })
@@ -19,6 +18,10 @@ describe('Pruebas en el uploadImage', () => {
         expect(typeof url).toBe('string')
 
         //Tomar el ID de la imagen
-        console.log(url)
+        const segments = url.split('/')
+        const imageId = segments[segments.length - 1].replace('.jpg', '')
+        cloudinary.v2.api.delete_resources(imageId, {}, () => {
+            done()
+        })
     })
 })
