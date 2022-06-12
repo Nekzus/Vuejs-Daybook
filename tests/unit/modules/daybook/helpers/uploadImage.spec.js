c73ba4a576750e22349ace050d240ecd79720b38
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-06-12 19:36:11 -0300

    Testing autenticado

--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -6,37 +6,28 @@ import uploadImage from '@/modules/daybook/helpers/uploadImage'
 cloudinary.config({
     cloud_name: 'dx0pryfzn',
     api_key: '422916932349318',
-    api_secret: 'gM_vs-URpSAyA3xV-PsoTg8xF3M'
+    api_secret: 'gM_vs-URpSAyA3xV-PsoTg8xF3M',
 })
 
-
 describe('Pruebas en el uploadImage ', () => {
-    
-    test('debe de cargar un archivo y retornar el url', async( done ) => {
-        
-        const { data } = await axios.get('https://res.cloudinary.com/dx0pryfzn/image/upload/v1627077252/ip3vwn8sjvnncphqka4y.jpg', {
-            responseType: 'arraybuffer'
-        })
+    test('debe de cargar un archivo y retornar el url', async done => {
+        const { data } = await axios.get(
+            'https://res.cloudinary.com/dsvsl0b0b/image/upload/v1653346900/uu4crmvkmv2tk3ntze9y.jpg', {
+                responseType: 'arraybuffer',
+            },
+        )
 
-        const file = new File([ data ], 'foto.jpg')
+        const file = new File([data], 'foto.jpg')
 
-        const url = await uploadImage( file )
+        const url = await uploadImage(file)
 
-        expect( typeof url ).toBe('string')
-
+        expect(typeof url).toBe('string')
 
         // Tomar el ID
         const segments = url.split('/')
-        const imageId = segments[ segments.length - 1 ].replace('.jpg','')
-        cloudinary.v2.api.delete_resources( imageId, {}, () => {
+        const imageId = segments[segments.length - 1].replace('.jpg', '')
+        cloudinary.v2.api.delete_resources(imageId, {}, () => {
             done()
         })
-
-        
     })
-    
-
 })
-
-
-
